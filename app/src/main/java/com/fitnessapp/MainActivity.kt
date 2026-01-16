fa00ccb0f5b7858175993e99490ea7cb692dd1ed
Grey <416699033lml@gmail.com>
2026-01-16 19:07:43 +0800

    Update for packing to apk

--- app/src/main/java/com/fitnessapp/MainActivity.kt
+++ app/src/main/java/com/fitnessapp/MainActivity.kt
@@ -3,36 +3,17 @@ package com.fitnessapp
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.viewinterop.AndroidView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MaterialTheme {
-                WebWorkoutApp()
-            }
+        val webView = WebView(this).apply {
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            loadUrl("file:///android_asset/web/index.html")
         }
+        setContentView(webView)
     }
-}
-
-@Composable
-fun WebWorkoutApp() {
-    AndroidView(
-        modifier = Modifier.fillMaxSize(),
-        factory = { context ->
-            WebView(context).apply {
-                webViewClient = WebViewClient()
-                settings.javaScriptEnabled = true
-                settings.domStorageEnabled = true
-                loadUrl("file:///android_asset/web/index.html")
-            }
-        }
-    )
 }
